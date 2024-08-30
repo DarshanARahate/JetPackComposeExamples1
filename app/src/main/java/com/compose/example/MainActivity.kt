4b9b1c2a38c059dd93e9240c4ab3e35392e729bf
darshanrahate <rahate.darshan@gmail.com>
2024-08-30 17:29:21 +0530

    Text, TextField, Button, Image composable committed.

--- app/src/main/java/com/compose/example/MainActivity.kt
+++ app/src/main/java/com/compose/example/MainActivity.kt
@@ -3,17 +3,33 @@ package com.compose.example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.compose.example.ui.theme.JetPackComposeExamples1Theme
 
@@ -27,8 +43,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    Greeting("Android")
-                    TextFunction()
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                    ) {
+                        Greeting("Android")
+                        Spacer(modifier = Modifier.height(5.dp))
+                        TextFunction()
+                        Spacer(modifier = Modifier.height(5.dp))
+                        ImageFunction()
+                        Spacer(modifier = Modifier.height(5.dp))
+                        ButtonFunction()
+                        Spacer(modifier = Modifier.height(5.dp))
+                        TextInput()
+                    }
+
                 }
             }
         }
@@ -47,19 +77,78 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     JetPackComposeExamples1Theme {
-//        Greeting("Android")
-          TextFunction()
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Greeting("Android")
+            Spacer(modifier = Modifier.height(5.dp))
+            TextFunction()
+            Spacer(modifier = Modifier.height(5.dp))
+            ImageFunction()
+            Spacer(modifier = Modifier.height(5.dp))
+            ButtonFunction()
+            Spacer(modifier = Modifier.height(5.dp))
+            TextInput()
+        }
     }
 }
 
 @Composable
 private fun TextFunction() {
-    Text(text = "Hello World",
+    Text(
+        text = "Hello World",
         fontStyle = FontStyle.Italic,
         fontWeight = FontWeight.ExtraBold,
         color = Color.Red,
         fontSize = 16.sp,
-        textAlign = TextAlign.Left)
+        textAlign = TextAlign.Left
+    )
 }
 
+@Composable
+private fun ImageFunction() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_heart),
+        contentDescription = "Dummy Image",
+        colorFilter = ColorFilter.tint(Color.Blue),
+        contentScale = ContentScale.Fit
+    )
+}
 
+@Composable
+private fun ButtonFunction() {
+    Button(
+        onClick = { },
+        colors = ButtonDefaults.buttonColors(
+            contentColor = Color.White
+        ),
+        enabled = true
+    ) {
+        Text(
+            text = "Hello",
+            modifier = Modifier.width(35.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_heart),
+            contentDescription = "Dummy"
+        )
+    }
+}
+
+@Composable
+private fun TextInput() {
+    val state = remember {
+        mutableStateOf("")
+    }
+
+    TextField(value = state.value,
+        onValueChange = {
+            state.value = it
+        },
+        label = { Text(text = "Enter Message") }
+    )
+
+}
