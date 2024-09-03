0e3d8236aed1df227673e72f84cea328016bf209
darshanrahate <rahate.darshan@gmail.com>
2024-09-03 18:59:24 +0530

    Recomposition

--- app/src/main/java/com/compose/example/MainActivity.kt
+++ app/src/main/java/com/compose/example/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.compose.example
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.compose.example.ui.theme.JetPackComposeExamples1Theme
@@ -22,7 +28,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    Column {
+                        Recomposable()
+                    }
+
                 }
             }
         }
@@ -42,5 +52,19 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     JetPackComposeExamples1Theme {
         Greeting("Android")
+    }
+}
+
+@Composable
+public fun Recomposable() {
+    val state = remember {
+        mutableStateOf(0.0)
+    }
+    Log.d("TAGGED", "Logged during Initial composition")
+    Button(onClick = {
+        state.value = Math.random()
+    }) {
+        Log.d("TAGGED", "Logged during Initial composition and Recomposition")
+        Text(text = state.value.toString())
     }
 }
