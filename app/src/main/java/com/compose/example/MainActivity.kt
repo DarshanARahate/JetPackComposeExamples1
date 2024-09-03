77a142d5c2ebe4ceb09e00ec3b4780bd95df7846
darshanrahate <rahate.darshan@gmail.com>
2024-09-03 14:30:31 +0530

    Row Columns Layout

--- app/src/main/java/com/compose/example/MainActivity.kt
+++ app/src/main/java/com/compose/example/MainActivity.kt
@@ -3,13 +3,22 @@ package com.compose.example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.compose.example.ui.theme.JetPackComposeExamples1Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    ListViewItem()
                 }
             }
         }
@@ -41,6 +51,38 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     JetPackComposeExamples1Theme {
-        Greeting("Android")
+//        Greeting("Android")
+        ListViewItem()
+    }
+}
+
+@Composable
+private fun ListViewItem() {
+    Column {
+        Spacer(modifier = Modifier.size(5.dp))
+        LayoutFunction(imgId = R.drawable.ic_heart, name = "John Doe", occupation = "Software Engineer")
+        Spacer(modifier = Modifier.size(5.dp))
+        LayoutFunction(imgId = R.drawable.ic_heart, name = "Annie Shaw", occupation = "Technical Lead")
+        Spacer(modifier = Modifier.size(5.dp))
+        LayoutFunction(imgId = R.drawable.ic_heart, name = "James Leo", occupation = "Lead Engineer")
+        Spacer(modifier = Modifier.size(5.dp))
+        LayoutFunction(imgId = R.drawable.ic_heart, name = "Brad Shane", occupation = "Software Engineer")
+
+    }
+}
+
+@Composable
+private fun LayoutFunction(imgId: Int, name: String, occupation: String) {
+    Row {
+        Image(
+            painter = painterResource(id = imgId),
+            contentDescription = "",
+            Modifier.size(40.dp)
+        )
+
+        Column {
+            Text(text = name, fontWeight = FontWeight.Bold)
+            Text(text = occupation, fontWeight = FontWeight.Thin, fontSize = 12.sp)
+        }
     }
 }
