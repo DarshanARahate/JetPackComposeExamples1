fefce623e9bb60883bdb0d6a92d11152fc7082aa
darshanrahate <rahate.darshan@gmail.com>
2024-09-04 21:20:57 +0530

    Compose State Concept - Hoisting & Unidirectional Flow

--- app/src/main/java/com/compose/example/MainActivity.kt
+++ app/src/main/java/com/compose/example/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.compose.example
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.compose.example.ui.theme.JetPackComposeExamples1Theme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +38,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    NotificationScreen()
                 }
             }
         }
@@ -42,5 +58,49 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     JetPackComposeExamples1Theme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun NotificationScreen() {
+    var count: MutableState<Int> = rememberSaveable { mutableStateOf(0) }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(1f)
+    ) {
+        NotificationCounter(count.value) { count.value++ }
+        MessageBar(count.value)
+    }
+}
+
+@Composable
+fun NotificationCounter(count: Int, increment: () -> Int) {
+
+    Column(verticalArrangement = Arrangement.Center) {
+        Text(text = "You have sent ${count} notification")
+        Button(onClick = {
+            increment()
+            Log.d("CODERSTAG", "Button Clicked")
+        }) {
+            Text(text = "Send Notification")
+        }
+    }
+}
+
+@Composable
+fun MessageBar(count: Int) {
+    Card {
+        Row(
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                imageVector = Icons.Outlined.Favorite,
+                contentDescription = "",
+                Modifier.padding(4.dp)
+            )
+            Text(text = "Messages Sent so far - $count")
+        }
     }
 }
